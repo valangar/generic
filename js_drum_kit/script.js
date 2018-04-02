3f3fe11b5b3b670e844f09d2d4a5f1f4d1480ad6
valangar <varsha.alangar@gmail.com>
2018-04-02 11:44:31 -0700

    Added key event, audio and transition css

--- js_drum_kit/script.js
+++ js_drum_kit/script.js
@@ -1,5 +1,23 @@
 //Author: Varsha Alangar
 //CREATED ON: 03/22/2018
-//LAST MODIFIED ON: 03/22/2018
+//LAST MODIFIED ON: 04/02/2018
 //DESCRIPTION: Creating a page using Vanilla JS that allows user to press keys to generate drum sounds. 
+var keys = Array.from(document.querySelectorAll(".key"));
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+window.addEventListener("keydown", playSound);
+
+function playSound(ele) {
+	var key = document.querySelector(`div[data-key = "${ele.keyCode}"]`);
+	var audio = document.querySelector(`audio[data-key = "${ele.keyCode}"]`);
+	key.classList.add("playing");
+	audio.currentTime = 0;
+	audio.play();
+}
+function removeTransition(ele) {
+	if(ele.propertyName !== 'transform') {
+		return;
+	}
+	console.log("remove");
+	ele.target.classList.remove("playing");
+}
 
